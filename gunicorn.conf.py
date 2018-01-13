9eba4d063280047ffab3e051d68a1d6e40eda172
Mykhailo Keda <mriynuk@gmail.com>
2018-01-13 15:57:35 +0200

    Updated gunicorn.conf.py (replaced sock with port).

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,11 @@
 """Gunicorn config."""
-bind = 'unix:/uwsgi/airtickets.sock'
+# TODO: Doesn't work, need to check why.
+# bind = 'unix:/uwsgi/airtickets.sock'
+bind = '127.0.0.1:8001'
 workers = 2
 timeout = 30
 max_requests = 100
 daemon = False
-umask = '644'
+# umask = '644'
 user = 'nobody'
 loglevel = 'info'
