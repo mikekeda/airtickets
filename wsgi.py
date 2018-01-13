de1a358e51be1daed6bb6166f72809a80416761d
Mykhailo Keda <mriynuk@gmail.com>
2018-01-13 11:29:39 +0200

    Added except KeyboardInterrupt to make supervisord works.

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,7 @@ from app import app
 import bjoern
 
 if __name__ == "__main__":
-    bjoern.run(app, 'unix:/uwsgi/airtickets.sock')
+    try:
+        bjoern.run(app, 'unix:/uwsgi/airtickets.sock')
+    except KeyboardInterrupt:
+        pass
