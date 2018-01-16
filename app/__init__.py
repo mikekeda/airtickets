47b6241f4bdae78ca5f4a1b0f5677e7ed9508be6
Mykhailo Keda <mriynuk@gmail.com>
2018-01-16 21:14:15 +0200

    Fixed db.init_app(app).

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 from .models import (City, LanguageScript, CityName, Airport, Airline,
                      Route, NeoAirport, NeoRoute)
+from extensions import db
 
 app = Flask(__name__)
 app.config.from_object('app.config.DefaultConfig')
@@ -11,5 +12,7 @@ toolbar = DebugToolbarExtension(app)
 app.jinja_env.globals['static'] = (
     lambda filename: url_for('static', filename=filename)
 )
+
+db.init_app(app)
 
 from app import views, models
